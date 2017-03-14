63411a98cda58a1e0a0db59459ec93e376a63b0e
loulou123546 <loulou.valleres@live.fr>
2017-03-14 18:43:23 +0100

    ajout du menu

---
+++ Menu/menu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour {
+
+	public void loadscene (int index) {
+		SceneManager.LoadScene(index);
+	}
+}
